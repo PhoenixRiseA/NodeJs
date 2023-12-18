92e49f94dcc4237bc30243d96288475729af22d2
sailalith <sailalith.raju@conversight.ai>
2023-12-18 08:23:24 +0530

    using express, handling routes and parsing incoming reuqests

--- app.js
+++ app.js
@@ -1,23 +1,20 @@
-const http = require('http');
 
 const express = require('express');
-
+const bodyParser = require('body-parser');
 const app = express();
-app.use('/',(req, res, next)=>{
-  console.log("This always runs");
-  next();
-});
-
+app.use(bodyParser.urlencoded({extended:false}));
 app.use('/add-product',(req, res, next)=>{
-  console.log("In add product middle ware");
-  res.send("<h1>Add Product</h1>"); // Allows us to send a response
+
+  res.send("<form action='/product' method='POST'><input type='text' name='title'><button type='submit'>Add product</button></form>"); // Allows us to send a response
 });
 
+app.use('/product',(req,res,next)=>{
+  console.log(req.body);
+  res.redirect('/');
+})
+
 app.use('/',(req, res, next)=>{
-  console.log("In another middle ware");
   res.send("<h1>Hello from express</h1>"); // Allows us to send a response
 });
 
-const server = http.createServer(app);
-
-server.listen(4000);
+app.listen(4000);
